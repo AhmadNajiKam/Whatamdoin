9efd3c691063ecd6916e79cc48fce458746abed3
Ahmad Naji <ahmadnajikam@gmail.com>
2025-09-04 22:00:57 +0300

    Create a simple schema with a simple query for it in the server, also create a template for the front end in react

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,24 @@
+import { Client } from "pg";
 import app from "./app";
+const client = new Client({
+	user: "postgres",
+	password: "whereami",
+	host: "localhost",
+	port: 5432,
+	database: "whereami",
+});
+client.connect();
+const values = [
+	"0786296839",
+	"ahmadnaji",
+	"ahmadnajikam@gmail.com",
+	"majican@14533",
+	"busy",
+];
+await client.query("INSERT INTO users VALUES ($1,$2,$3,$4,$5)", values);
+
+const res = await client.query("SELECT * FROM users");
+console.log(res.rows);
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
